f6426a28f974197fac8ed6ad7bbccbe01b4b25dd
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-24 16:59:00 +0200

    fix: fix problem with field 'distance'

--- airport/serializers.py
+++ airport/serializers.py
@@ -16,6 +16,7 @@ class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
         fields = (
+            "id",
             "route",
             "airplane",
             "departure_time",
@@ -27,25 +28,34 @@ class FlightSerializer(serializers.ModelSerializer):
 class CrewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Crew
-        fields = ("first_name", "last_name", "role", "full_name")
+        fields = ("id", "first_name", "last_name", "role", "full_name")
 
 
 class RouteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Route
-        fields = ("source", "destination", "distance")
+        fields = ("id", "source", "destination", "distance")
+        read_only_fields = ("id", "distance")
+
+    def create(self, validated_data):
+        route = Route(**validated_data)
+        route.clean()
+        route.save()
+
+        return route
 
 
 class AirportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airport
-        fields = ("name", "closest_big_city")
+        fields = ("id", "name", "closest_big_city")
 
 
 class AirplaneSerializer(serializers.ModelSerializer):
     class Meta:
         model = Airplane
         fields = (
+            "id",
             "name",
             "rows",
             "seats_in_row",
@@ -53,21 +63,22 @@ class AirplaneSerializer(serializers.ModelSerializer):
             "image",
             "capacity"
         )
+        read_only_fields = ("id", "airplane_type")
 
 
 class AirplaneTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirplaneType
-        fields = ("name",)
-
+        fields = ("id", "name",)
+        read_only_fields = ("id", "name")
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
-        fields = ("row", "seat", "flight", "order")
+        fields = ("id", "row", "seat", "flight", "order")
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ("created_at", "user")
+        fields = ("id", "created_at", "user")
