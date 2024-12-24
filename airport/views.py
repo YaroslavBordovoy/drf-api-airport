6c6f4ebf262637049f93b084172c8244908b77d1
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2024-12-24 15:19:39 +0200

    feat: add basic serilizers and views

--- airport/views.py
+++ airport/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from airport.models import (
+    Flight,
+    Crew,
+    Route,
+    Airport,
+    Airplane,
+    AirplaneType,
+    Ticket,
+    Order,
+)
+from airport.serializers import (
+    FlightSerializer,
+    CrewSerializer,
+    RouteSerializer,
+    AirportSerializer,
+    AirplaneSerializer,
+    AirplaneTypeSerializer,
+    TicketSerializer,
+    OrderSerializer,
+)
+
+
+class FlightViewSet(viewsets.ModelViewSet):
+    queryset = Flight.objects.all()
+    serializer_class = FlightSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
+
+
+class AirportViewSet(viewsets.ModelViewSet):
+    queryset = Airport.objects.all()
+    serializer_class = AirportSerializer
+
+
+class AirplaneViewSet(viewsets.ModelViewSet):
+    queryset = Airplane.objects.all()
+    serializer_class = AirplaneSerializer
+
+
+class AirplaneTypeViewSet(viewsets.ModelViewSet):
+    queryset = AirplaneType.objects.all()
+    serializer_class = AirplaneTypeSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
--- core/urls.py
+++ core/urls.py
@@ -19,12 +19,13 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
+        path("api/airport/", include("airport.urls", namespace="airport")),
     ]
     + debug_toolbar_urls()
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
