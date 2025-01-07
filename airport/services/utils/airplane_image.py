792bc8d4f142464f27f545116aa77c347f4b0b1f
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-07 18:00:01 +0200

    fix: Fix airplane_image_file_path func

--- airport/services/utils/airplane_image.py
+++ airport/services/utils/airplane_image.py
@@ -10,4 +10,4 @@ def airplane_image_file_path(instance: Airplane, filename: str) -> str:
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join("uploads/movies/", filename)
+    return os.path.join("uploads", "movies", filename)
