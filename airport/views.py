2d3571774853099bcffc25781c2e54d5e9962b43
Yaroslav Bordovoy <yaroslavbordovoy@gmail.com>
2025-01-07 17:54:09 +0200

    fix: Delete unused import

--- airport/views.py
+++ airport/views.py
@@ -2,7 +2,6 @@ from django.db.models import Count, F
 from django_filters import rest_framework as filters
 from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
-from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 
 from airport.filters import (
